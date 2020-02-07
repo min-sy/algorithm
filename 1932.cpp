ddedbb470c135f48e372abd85df64fb45594f2da
min-sy <nsy728@ewhain.net>
2020-02-07 20:39:35 +0900

    정수삼각형

--- 1932.cpp
+++ 1932.cpp
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #define MAX(a,b) ((a)>(b)?(a):(b))
 
-int cal(int arr[][500], int n, int index, int size) {
-	if (n == 1) return arr[size - n][index];
-	int left = cal(arr, n - 1, index, size);
-	int right = cal(arr, n - 1, index+1, size);
-	return arr[size - n][index] + MAX(left, right);
-}
 
 int main(void) {
-	int n;
-	int arr[500][500];
+	int dp[501][501] = {};
+	int n, max=0;
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j <= i; j++) {
-			scanf("%d", &arr[i][j]);
+	for (int i = 1; i <=n; i++) {
+		for (int j = 1; j <= i; j++) {
+			scanf("%d", &dp[i][j]);
+			if (j == 1)
+				dp[i][j] += dp[i - 1][j];
+			else if (i == j)
+				dp[i][j] += dp[i - 1][i - 1];
+			else
+				dp[i][j] += MAX(dp[i - 1][j - 1], dp[i - 1][j]);
+
+			if (max < dp[i][j]) max = dp[i][j];
 		}
 	}
-	int left = cal(arr, n - 1, 0, n);
-	int right = cal(arr, n - 1, 1, n);
-	int result = arr[0][0] + MAX(left,right);
-	printf("%d", result);
-	return 0;
+
+	printf("%d", max);
 }
