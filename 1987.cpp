83e12fd2c877bceb027907394b9fa822615f2df3
min-sy <nsy728@ewhain.net>
2020-02-10 21:01:31 +0900

    알파벳

--- 1987.cpp
+++ 1987.cpp
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 int visited[26];
-int count=0;
 char alpha[20][20];
+int answer=1, count=1;
 int direction[4][2] = { {0,1},{0,-1},{1,0},{-1,0} };
 
 void travel(int x, int y, int r, int c) {
 	visited[alpha[x][y] - 'A'] = 1;
-	count++;
 	for (int i = 0; i < 4; i++) {
 		int newx = x + direction[i][0];
 		int newy = y + direction[i][1];
 		if (newx < 0 || newx >= r || newy < 0 || newy >= c) continue;
 		if (visited[alpha[newx][newy] - 'A']) continue;
+		count++;
+		answer = answer > count ? answer : count;
 		travel(newx, newy, r, c);
 	}
+	count--;
+	visited[alpha[x][y] - 'A'] = 0;
 }
 
 int main(void) {
@@ -26,7 +29,8 @@ int main(void) {
 			scanf(" %c", &alpha[i][j]);
 		}
 	}
+
 	travel(0, 0, r, c);
-	printf("%d", count);
+	printf("%d\n", answer);
 	return 0;
 }
